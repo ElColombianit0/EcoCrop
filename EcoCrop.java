cd1fa2ba4d1966f2d80210e1c1d2697f5afc8b0b
ElColombianit0 <sergioadresp1@gmail.com>
2024-08-29 13:09:38 -0500

    commit renamed:    Mavenproject2.java -> EcoCrop.java

--- EcoCrop.java
+++ EcoCrop.java
@@ -1,6 +1,6 @@
 package EcoCrop;
 import java.util.Scanner;
-public class Mavenproject2 {
+public class EcoCrop {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner (System.in);
