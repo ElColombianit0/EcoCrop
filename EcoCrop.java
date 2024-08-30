241ba9157af426daee820e202acd5fc08c9840b1
ElColombianit0 <sergioadresp1@gmail.com>
2024-08-30 14:14:48 -0500

    modified:   EcoCrop.java

--- EcoCrop.java
+++ EcoCrop.java
@@ -1,8 +1,8 @@
 package EcoCrop;
 import java.util.Scanner;
-class EcoCrop {
+public class EcoCrop {
 
-    public static void main(string[] args){
+    public static void main(String [] args){
         Scanner sc=new Scanner (System.in);
         
         System.out.println("MALPARIDOS HIJOS DE PUTA LOS QUE VEAN ESTO");
